b092204abff88380589ce403000c438628b2a9e4
Markku Heikkilä <markku.heikkila@cgi.com>
2025-02-25 12:30:41 +0200

    Created project

---
+++ SecureDemo/src/main/java/dev/vaakneri/SecureDemo/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package dev.vaakneri.SecureDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to the Secure Demo!";
+	}
+}
